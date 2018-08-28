6df6fa8306cd70e82b7e8bdbf71e63638f30a2d3
Bran Liang <lby89757@hotmail.com>
2018-08-28 18:39:24 +0800

    Add minimum style for editor

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Demo from './demo'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header>
+        <header className="header-wrappper">
           <h1>Demo</h1>
         </header>
-        <Demo />
+        <div className="demo-wrappper">
+          <Demo />
+        </div>
       </div>
     );
   }
