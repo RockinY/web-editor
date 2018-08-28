d9dcd81f651f39c8ae10900efa2084d85c325700
Bran Liang <lby89757@hotmail.com>
2018-08-28 17:28:43 +0800

    Add plugin editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import PluginEditor from 'draft-js-plugins-editor'
 
 class Editor extends Component {
+  onChange = (editorState) => {
+    this.props.onChange(editorState)
+  }
+
   render () {
     return (
       <div className='draftjs-web-editor'>
-        Demo
+        <PluginEditor
+          editorState={this.props.editorState}
+          onChange={this.onChange}
+        />
       </div>
     )
   }
