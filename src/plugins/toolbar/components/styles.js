97dff69c97b5ad9054c89193105c2bd496fec241
Bran Liang <lby89757@hotmail.com>
2018-08-31 19:34:40 +0800

    Add basic style to the toolbar wrapper

--- src/plugins/toolbar/components/styles.js
+++ src/plugins/toolbar/components/styles.js
@@ -2,4 +2,11 @@ import styled from 'styled-components'
 
 export const ToolbarWrapper = styled.div`
   display: ${p => p.show ? 'flex' : 'none'};
+  left: 50%;
+  transform: translate(-50%) scale(1);
+  position: absolute;
+  border: 1px solid #ddd;
+  background: #fff;
+  z-index: 6000;
+  box-sizing: border-box;
 `
