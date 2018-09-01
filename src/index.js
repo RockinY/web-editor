7993bf7358239b784158c3f131f551e4729ada0e
Bran Liang <lby89757@hotmail.com>
2018-09-01 23:26:06 +0800

    Add basic global style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Editor from './components/Editor'
 import * as convert from './convert'
+import './styles/global'
 
 export const toJSON = convert.toJSON
 export const fromJSON = convert.fromJSON
