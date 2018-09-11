1fa6d9111218c973e25afc82f006a7263a7e9b47
Bran Liang <lby89757@hotmail.com>
2018-09-11 20:15:22 +0800

    Update the demo content

--- demo/src/raw.js
+++ demo/src/raw.js
@@ -1,19 +1,94 @@
 export default {
-  blocks: [
+  "blocks": [
     {
-      key: 'foo',
-      text: 'Hello world',
-      type: 'unstyled',
-      depth: 0,
-      inlineStyleRanges: [],
-      entityRanges: [
-        {offset: 6, length: 5, key: 0}
+      "key": "foo",
+      "text": "I am title",
+      "type": "header-one",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "2dfh3",
+      "text": "Life is good, enjoy it.",
+      "type": "blockquote",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "i5jk",
+      "text": "COLORFUL WORLD AND TEXT ",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [
+        {
+          "offset": 0,
+          "length": 8,
+          "style": "color-DE350B"
+        },
+        {
+          "offset": 9,
+          "length": 5,
+          "style": "color-0052CC"
+        },
+        {
+          "offset": 15,
+          "length": 3,
+          "style": "color-00B8D9"
+        },
+        {
+          "offset": 19,
+          "length": 4,
+          "style": "color-36B37E"
+        }
       ],
-      data: {}
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "6e7bs",
+      "text": "This person is awesome. CLICK ME",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [
+        {
+          "offset": 24,
+          "length": 8,
+          "key": 0
+        }
+      ],
+      "data": {}
+    },
+    {
+      "key": "eqe1l",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+    },
+    {
+      "key": "do0sa",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
     }
   ],
-  entityMap: {
-    0: { type: "LINK", mutability: "MUTABLE", data: {url: "https://dev.krae.cn"}}
+  "entityMap": {
+    "0": {
+      "type": "LINK",
+      "mutability": "MUTABLE",
+      "data": {
+        "url": "https://github.com/BranLiang"
+      }
+    }
   }
 }
-
